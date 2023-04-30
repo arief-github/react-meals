858d67702b8ea276d638bbee055f1bf8bbad266d
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-04-30 21:59:48 +0700

    adding bump animation

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,18 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from './HeaderCartButton.module.scss';
 import cart from '../../assets/cart.svg';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const [isBump, setIsBump] = useState(false);
   const cartContext = useContext(CartContext);
 
   const numberOfCartItems = cartContext.items.reduce((currNum, item) => {
     return currNum + item.amount
   }, 0);
+
+  const { items } = cartContext;
+  const btnClasses = `${styles.button} ${isBump ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if(items.length === 0) return;
+
+    setIsBump(true);
+
+    const timer = setTimeout(() => {
+      setIsBump(false)
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items])
   
   return (
-    <button className={`${styles.button}`} onClick={props.onClick}>
-        <img className={`${styles.icon}`} src={cart} alt='cart icon'/>
+    <button className={btnClasses} onClick={props.onClick}>
+        <img className={styles.icon} src={cart} alt='cart icon'/>
         <span>Your Cart</span>
         <span className={`${styles.badge}`}>{numberOfCartItems}</span>
     </button>
